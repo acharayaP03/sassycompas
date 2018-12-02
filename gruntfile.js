f3e84f3370ec72dc9ced1c012ab8b510c5130909
Prabhakar Acharya <acharyaP03@gmail.com>
2018-12-03 00:03:40 +1100

    added images

--- gruntfile.js
+++ gruntfile.js
@@ -4,36 +4,40 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.initConfig({
     //tasks 
-    uglify: {
-      my_target: {
-        files: {
-          //destination of finished: originl
-          '_/js/script.js': ['_/components/js/*.js']
-        } //files
-      } //my_target
-    }, //uglify
     compass: {
       dev: {
         options: {
-          config : 'config.rb'
-        }//options
-      }//dev
+          config: 'config.rb'
+        } //options
+      } //dev
     }, //compass
-    watch: {
-        options: {livereload : true},
-        script: {
-          files:  ['_/components/js/*.js'],
-          task: ['uglify']
-        },//scripts
-        sass: {
-          files: ['_/components/sass/*.scss'],
-          task: ['compass:dev']
-        },//sass
-        html: {
-          files:  ['*.html']
-        }//html
-    }//watch
-  }) //initConfig
+
+    uglify: {
+      my_target: {  
+        files: {
+          '_/js/script.js': ['_/components/js/*.js']
+        }
+      }
+    },
+       watch: {
+      options: { livereload: true },
+      sass: {
+        files: ['_/components/sass/*.scss'],
+        tasks: ['compass:dev']
+      }, //sass
+
+      js: {
+        files: ['_/components/js/*.js'],
+        tasks: ['uglify']
+      }, //js
+
+      html: {
+        files: ['*.html']
+      } //html
+    } //watch
+  }); //initConfig
+
   //registering task for grunt command
   grunt.registerTask('default', 'watch');
 } //exports
+
