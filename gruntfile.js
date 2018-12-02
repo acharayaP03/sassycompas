e443d9e9c0d18cd276905e4f6b3a9222c4150a7b
Prabhakar Acharya <acharyaP03@gmail.com>
2018-12-02 23:12:56 +1100

    added compass and config.rb to convert sass to css

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.initConfig({
+    //tasks 
     uglify: {
       my_target: {
         files: {
@@ -10,15 +12,26 @@ module.exports = function(grunt) {
         } //files
       } //my_target
     }, //uglify
+    compass: {
+      dev: {
+        options: {
+          config : 'config.rb'
+        }//options
+      }//dev
+    }, //compass
     watch: {
         options: {livereload : true},
         script: {
           files:  ['_/components/js/*.js'],
           task: ['uglify']
         },//scripts
+        sass: {
+          files: ['_/components/sass/*.scss'],
+          task: ['compass:dev']
+        },//sass
         html: {
           files:  ['*.html']
-        }
+        }//html
     }//watch
   }) //initConfig
   //registering task for grunt command
